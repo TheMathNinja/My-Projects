343766937e7c77410fd0da3faba12e54309164c0
Mike Filicicchia <33882347+TheMathNinja@users.noreply.github.com>
2025-08-22 11:36:50 -0400

    Add files via upload

--- Football-Analytics-Labs/PositionChange.R
+++ Football-Analytics-Labs/PositionChange.R
@@ -8,9 +8,10 @@ library(tidyverse)
 nflverse::nflverse_update(devel = TRUE)
 nflverse::nflverse_sitrep()
 ffscrapr::ffverse_sitrep()
+ffscrapr::clear_cache()
 
-ADL24 <- mfl_connect(season = 2024, league_id = 60206, user_agent = "mikeyfili", rate_limit_number = 3, rate_limit_seconds = 6)
-FAFL24 <- mfl_connect(season = 2024, league_id = 22686, user_agent = "mikeyfili", rate_limit_number = 3, rate_limit_seconds = 6)
+ADL25 <- mfl_connect(season = 2025, league_id = 60206, user_agent = "mikeyfili", rate_limit_number = 3, rate_limit_seconds = 6)
+FAFL25 <- mfl_connect(season = 2025, league_id = 22686, user_agent = "mikeyfili", rate_limit_number = 3, rate_limit_seconds = 6)
 
 #Create function to generate all players and current position changes in MFL league
 get_league_players <- function(conn) {
@@ -25,17 +26,17 @@ get_league_players <- function(conn) {
     left_join(., y = ffscrapr::mfl_players() %>%
                 rename(mfl_id = player_id,
                        mfl_default_pos = pos) %>%
-                select(c("mfl_id", "mfl_default_pos")),
+                dplyr::select(c("mfl_id", "mfl_default_pos")),
               by = c("mfl_id")) %>%
     relocate(mfl_default_pos, .after = league_pos) %>%
     # Join with nflreadr player IDs to update espn_id
     left_join(., y = nflreadr::load_ff_playerids() %>%
-                select(mfl_id, espn_id),
+                dplyr::select(mfl_id, espn_id),
               by = "mfl_id") %>%
     mutate(espn_id = as.integer(coalesce(espn_id.x, espn_id.y))) %>%
-    select(-espn_id.x, -espn_id.y)  # Clean up temporary columns
+    dplyr::select(-espn_id.x, -espn_id.y)  # Clean up temporary columns
   
-  # Build df showing current league position changes. Should match MFL URL https://www46.myfantasyleague.com/2024/csetup?L=60206&C=CHGPOS
+  # Build df showing current league position changes. Should match MFL URL https://www46.myfantasyleague.com/2025/csetup?L=60206&C=CHGPOS
   curr_pos_changes <- players %>%
     filter(league_pos != mfl_default_pos) %>%
     #Put in alphabetical order to match MFL desktop view
@@ -45,17 +46,17 @@ get_league_players <- function(conn) {
 }
 
 
-ADL_players_changes <- get_league_players(ADL24)
-ADL_players_2024 <- ADL_players_changes$players_df
+ADL_players_changes <- get_league_players(ADL25)
+ADL_players_2025 <- ADL_players_changes$players_df
 ADL_curr_pos_changes <- ADL_players_changes$curr_pos_changes_df
 
-FAFL_players_changes <- get_league_players(FAFL24)
-FAFL_players_2024 <- FAFL_players_changes$players_df
+FAFL_players_changes <- get_league_players(FAFL25)
+FAFL_players_2025 <- FAFL_players_changes$players_df
 FAFL_curr_pos_changes <- FAFL_players_changes$curr_pos_changes_df
 
 
 #Build df listing all current ESPN Fantasy positions.
-espn_players_2024 <- ffscrapr::espn_players() %>%
+espn_players_2025 <- ffscrapr::espn_players() %>%
   rename(espn_id = player_id,
          espn_pos = pos,
          espn_player_name = player_name,
@@ -69,25 +70,26 @@ espn_players_2024 <- ffscrapr::espn_players() %>%
          ))
 
 #Join ESPN player list to MFL's
-ADL_players_2024_with_espn <- ADL_players_2024 %>%
-  full_join(., y = espn_players_2024 %>%
-              select(c("espn_id",
+ADL_players_2025_with_espn <- ADL_players_2025 %>%
+  full_join(., y = espn_players_2025 %>%
+              dplyr::select(c("espn_id",
                        "espn_player_name",
                        "espn_team",
                        "espn_pos")),
             by = c("espn_id")) %>%
   relocate(espn_pos, .after = mfl_default_pos)
 
-ADL_espn_disagreements <- ADL_players_2024_with_espn %>%
-  filter(league_pos != espn_pos)
+MFL_espn_disagreements <- ADL_players_2025_with_espn %>%
+  filter(mfl_default_pos != espn_pos)  %>%
+  arrange(mfl_player_name)
 
-view(ADL_espn_disagreements)
+view(MFL_espn_disagreements)
 
-ADL_FAFL_disagreements <- ADL_players_2024 %>%
+ADL_FAFL_disagreements <- ADL_players_2025 %>%
   rename(ADL_pos = league_pos) %>%
-  left_join(., y = FAFL_players_2024 %>%
+  left_join(., y = FAFL_players_2025 %>%
               rename(FAFL_pos = league_pos) %>%
-              select(mfl_id,
+              dplyr::select(mfl_id,
                      FAFL_pos),
             by = "mfl_id") %>%
   filter(ADL_pos != FAFL_pos) %>%
